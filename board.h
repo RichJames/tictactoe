e987cbc73f751b357f46994a11e629f9a7d94aca
RichJames <risajajr@gmail.com>
2019-10-25 09:37:46 -0500

    Refactored board array to be single dimension.

--- board.h
+++ board.h
@@ -13,31 +13,27 @@ public:
   {
     // display the board
     std::cout << '\n'
-              << ' ' << _board[0][0] << " | " << _board[0][1] << " | " << _board[0][2] << '\n'
+              << ' ' << _board[0] << " | " << _board[1] << " | " << _board[2] << '\n'
               << "-----------" << '\n'
-              << ' ' << _board[1][0] << " | " << _board[1][1] << " | " << _board[1][2] << '\n'
+              << ' ' << _board[3] << " | " << _board[4] << " | " << _board[5] << '\n'
               << "-----------" << '\n'
-              << ' ' << _board[2][0] << " | " << _board[2][1] << " | " << _board[2][2] << '\n'
+              << ' ' << _board[6] << " | " << _board[7] << " | " << _board[8] << '\n'
               << '\n';
   }
 
   bool mark_move(int position, char move)
   {
     // update the board with the move
-    // 1 = [0][0], 2 = [0][1], 3 = [0][2]
-    // 4 = [1][0], 5 = [1][1], 6 = [1][2]
-    // 7 = [2][0], 8 = [2][1], 9 = [2][2]
+    // 1 = [0], 2 = [1], 3 = [2]
+    // 4 = [3], 5 = [4], 6 = [5]
+    // 7 = [6], 8 = [7], 9 = [8]
 
-    // row = (position-1)/3 - 1.  Ex.  int((9-1)/3) = 2; int((3-1)/3) = 0; int((5-1)/3) = 1
-    // column = (position-1) % 3 - 1.  Ex. (1-1) % 3 = 0; (9-1) % 3 = 2; (5-1) % 3 = 1
-
-    int row = (position - 1) / 3;
-    int col = (position - 1) % 3;
+    position -= 1; // User position is 1-based, array position is 0-based.
 
     // is move legal?
-    if (_board[row][col] == ' ')
+    if ((position >= 0 && position <= 8) && _board[position] == ' ')
     {
-      _board[row][col] = move;
+      _board[position] = move;
       return true;
     }
 
@@ -51,44 +47,40 @@ public:
 
     // determince if there is a winner
     // check rows
-    for (int i = 0; i < 3; ++i)
+    for (int i = 0; i < 9; i += 3)
     {
-      if ((_board[i][0] == _board[i][1]) && (_board[i][0] == _board[i][2]) && (_board[i][0] != ' '))
+      if ((_board[i] == _board[i + 1]) && (_board[i] == _board[i + 2]) && (_board[i] != ' '))
         return {true, true};
     }
     // check columns
     for (int i = 0; i < 3; ++i)
     {
-      if ((_board[0][i] == _board[1][i]) && (_board[0][i] == _board[2][i]) && (_board[0][i] != ' '))
+      if ((_board[i] == _board[i + 3]) && (_board[i] == _board[i + 6]) && (_board[i] != ' '))
         return {true, true};
     }
     // check diagonals
-    if ((_board[0][0] == _board[1][1]) && (_board[0][0] == _board[2][2]) && (_board[0][0] != ' '))
+    if ((_board[0] == _board[4]) && (_board[0] == _board[8]) && (_board[0] != ' '))
       return {true, true};
-    if ((_board[0][2] == _board[1][1]) && (_board[0][2] == _board[2][0]) && (_board[0][2] != ' '))
+    if ((_board[2] == _board[4]) && (_board[2] == _board[6]) && (_board[2] != ' '))
       return {true, true};
 
     // check for draw
-    for (int i = 0; i < 3; ++i)
-      for (int j = 0; j < 3; ++j)
-        if (_board[i][j] == ' ') // did we find an unplayed space?
-          return {false, false}; // game doesn't end, no winner found
+    for (int i = 0; i < 9; ++i)
+      if (_board[i] == ' ')    // did we find an unplayed space?
+        return {false, false}; // game doesn't end, no winner found
 
     return {true, false}; // game ends, no winner found
   }
   void reset()
   {
-    for (int i = 0; i < 3; ++i)
-      for (int j = 0; j < 3; ++j)
-        _board[i][j] = ' ';
+    for (int i = 0; i < 9; ++i)
+      _board[i] = ' ';
   }
 
 private:
   // implement this as a std::vector? or std::array?
-  std::array<std::array<char, 3>, 3> _board{{{' ', ' ', ' '}, {' ', ' ', ' '}, {' ', ' ', ' '}}};
-  //   char _board[3][3] = {{' ', ' ', ' '},
-  //                        {' ', ' ', ' '},
-  //                        {' ', ' ', ' '}};
+  // std::array<std::array<char, 3>, 3> _board{{{' ', ' ', ' '}, {' ', ' ', ' '}, {' ', ' ', ' '}}};
+  std::array<char, 9> _board{' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
 };
 
 #endif // ___BOARD_H
