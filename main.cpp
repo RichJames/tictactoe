a14dd03e4b5df075693943dd94185322ae2b6e77
RichJames <risajajr@gmail.com>
2019-10-26 21:39:43 -0500

    Improved name usage, plus created TODOs

--- main.cpp
+++ main.cpp
@@ -70,7 +70,7 @@ void play_game(const pPlayer &player1, const pPlayer &player2, const std::shared
     auto [game_end, winner_found] = pBoard->check_for_winner();
     if (game_end && winner_found)
     {
-      std::cout << "Game ends.  We have a winner!\n";
+      std::cout << "Game ends.  " << currentPlayer->getName() << " wins!\n";
       break;
     }
     if (game_end && !winner_found)
@@ -88,6 +88,7 @@ void play_game(const pPlayer &player1, const pPlayer &player2, const std::shared
 #define NORMAL_RUN
 // #define TEST_RUN
 // #define TEST_WINNER_DETECTION
+// #define PRINT_PLAYER_INFO
 
 int main(/* int argc, char const *argv[]*/)
 {
@@ -103,7 +104,6 @@ int main(/* int argc, char const *argv[]*/)
 #endif // TEST_RUN
 
 #ifdef TEST_WINNER_DETECTION
-  // FIX:  Need a switch or something here to control if/when we run these tests
   if (test_board(ptrBoard))
   {
     std::cout << "**** All tests passed!!! ****\n";
@@ -118,9 +118,9 @@ int main(/* int argc, char const *argv[]*/)
   std::cout << "Welcome to Tic Tac Toe!" << std::endl
             << std::endl;
 
-  Player_Type first_type = ChoosePlayer("Who will be player 1 (1=human, 2=computer)? ");
+  Player_Type first_player_type = ChoosePlayer("Who will be player 1 (1=human, 2=computer)? ");
   std::string firstplayername;
-  if (first_type == Player_Type::human)
+  if (first_player_type == Player_Type::human)
   {
     firstplayername = GetPlayerName("Enter player 1's name: ");
   }
@@ -129,29 +129,24 @@ int main(/* int argc, char const *argv[]*/)
     firstplayername = "Computer 1";
   }
 
-  Player_Type second_type = ChoosePlayer("\nWho will be player 2 (1=human, 2=computer)? ");
+  Player_Type second_player_type = ChoosePlayer("\nWho will be player 2 (1=human, 2=computer)? ");
   std::string secondplayername;
-  if (second_type == Player_Type::human)
+  if (second_player_type == Player_Type::human)
   {
     secondplayername = GetPlayerName("Enter player 2's name: ");
   }
   else
   {
-    if (firstplayername == "Computer 1")
-    {
-      secondplayername = "Computer 2";
-    }
-    else
-    {
-      secondplayername = "Computer 1";
-    }
+    secondplayername = firstplayername == "Computer 1" ? "Computer 2" : "Computer 1";
   }
 
-  pPlayer player1 = assignPlayer(firstplayername, first_type, Player_Order::first_player, ptrBoard);
-  pPlayer player2 = assignPlayer(secondplayername, second_type, Player_Order::second_player, ptrBoard);
+  pPlayer player1 = assignPlayer(firstplayername, first_player_type, Player_Order::first_player, ptrBoard);
+  pPlayer player2 = assignPlayer(secondplayername, second_player_type, Player_Order::second_player, ptrBoard);
 
-  // player1->Info();
-  // player2->Info();
+#ifdef PRINT_PLAYER_INFO
+  player1->Info();
+  player2->Info();
+#endif // PRINT_PLAYER_INFO
 
   play_game(player1, player2, ptrBoard);
 #endif // NORMAL_RUN
